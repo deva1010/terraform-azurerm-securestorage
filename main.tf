2fec529270ac96ccb8a5d47aeae3b3b3b92e9382
Devesh Shringi <devesh.shringi@gmail.com>
2025-11-21 19:18:03 +0530

    adding storage module

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.13.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.54.0"
+    }
+  }
+}
+
+locals {
+  tags = {
+    "Environment" = var.environment
+  }
+}
+
+resource "azurerm_storage_account" "securestorage" {
+  name                          = var.storage_account_name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  account_tier                  = "Standard"
+  account_replication_type      = var.environment == "Production" ? "GRS" : "LRS"
+  public_network_access_enabled = false
+
+  tags = local.tags
+}
